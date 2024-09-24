e8793b72a64e9c33ab1717965d50fb2c3697a1a7
lydia a <lydie.antonova@gmail.com>
2024-09-24 22:50:25 +0200

    setup and add PlaceCard

---
+++ src/components/PlaceCard/PlaceCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {PlaceCardContainer} from './PlaceCard.styled'
+
+function Placecard() {
+  return (
+    <>
+
+    <div>Placecard</div>
+    <PlaceCardContainer></PlaceCardContainer>
+
+    </>
+  )
+}
+
+export default Placecard
---
+++ src/components/PlaceCard/PlaceCard.styled.jsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const PlaceCardContainer = styled.div`
+    width: 150px;
+    height: 150px;
+    display: block;
+`
